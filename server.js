1fe8b173feb81c451499dcb12437ffa2a4da9200
JayFrank\FDC.JayFrank-NC-FE <fdc.jayfrankbarquin@gmail.com>
2025-05-16 13:30:16 +0800

    pre dep

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ dotenv.config();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173', // Adjust for production
+    origin: 'https://taskmanagement-cream.netlify.app', // Adjust for production
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
@@ -27,7 +27,7 @@ const io = new Server(server, {
 app.set('socketio', io);
 // const io = req.app.get('socketio'); <-- use this in your routes to access the io instance
 // app.use(cors({origin: 'https://taskmanagement-cream.netlify.app', methods: 'GET, POST, PUT, DELETE', allowedHeaders: 'Content-Type, Authorization', credentials: true}));
-app.use(cors({origin: 'http://localhost:5173', methods: 'GET, POST, PUT, DELETE', allowedHeaders: 'Content-Type, Authorization', credentials: true}));
+app.use(cors({origin: 'https://taskmanagement-cream.netlify.app', methods: 'GET, POST, PUT, DELETE', allowedHeaders: 'Content-Type, Authorization', credentials: true}));
 // app.use(cors({origin: '*', methods: 'GET, POST, PUT, DELETE', allowedHeaders: '*', credentials: true}));
 
 app.use(express.json());
